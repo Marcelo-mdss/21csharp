4f4e18404d99f62b5d8f29b5a3093457fd83555c
Marcelo Dos Santos <marcelo_mdss@live.com>
2023-01-19 08:26:09 -0300

    Aplicação Estudo empresa Lina

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("===================[Seja Bem vindo a empresa Lina]======================");
+
+Console.WriteLine("""
+O que você deseja fazer ?
+1 - Cadastrar o cliente
+2 - Ver conta corrente
+3 - fazer credito em conta
+4 - Fazer débito em conta
+5 - sair do sistema
+""");
+
+string opcao = Console.ReadLine()?.Trim();
+
+
+
+
+
