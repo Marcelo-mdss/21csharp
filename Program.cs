873007a0ab74b1029df97ceba955179b20b22c67
Marcelo Dos Santos <marcelo_mdss@live.com>
2023-01-20 12:04:45 -0300

    Att2 empresa Luna

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿using Internal;
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
 using System;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("===================[Seja Bem vindo a empresa Lina]======================");
-
-
-
+bool sair = false;
 do{
 Console.WriteLine("""
 O que você deseja fazer ?
@@ -18,22 +17,46 @@ O que você deseja fazer ?
 string opcao = Console.ReadLine()?.Trim();
 Console.Clear();
 
-bool sair = false;
+
+List<string> global = new List<string>();
 
 switch(opcao)
 {
     case "1":
-    Console.WriteLine("Cadastro de Cliente");
-    List<string> cadastro = 
-    Console.clear();
+
+    Console.WriteLine("===========Cadastro do Cliente============");
+    
+    List<string> cadastro = new List<string>();
+    var id = Guid.NewGuid();
+    Console.WriteLine("000 GUID " + id);
+
+    Console.WriteLine("Digite seu Nome: ");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine($"Digite seu Telefone {nome}");
+    string Tel = Console.ReadLine();
+
+    Console.WriteLine($"Digite seu E-mail {nome}");
+    string email = Console.ReadLine();
+
+    cadastro.Add(id.ToString());
+    cadastro.Add(nome);
+    cadastro.Add(Tel);
+    cadastro.Add(email);
+
+    global.Add(cadastro.ToString());
+
+    Console.Clear();
     break;
 
     case "2":
+    Console.WriteLine(global);
     Console.Clear();
     break;
 
     case "3":
     Console.Clear();
+    break;
 
     case "4":
     Console.Clear();
@@ -45,7 +68,7 @@ switch(opcao)
     break;
 }
 
-}while(sair == false);
+}while(sair =! false);
 
 
 
